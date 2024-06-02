84006fe08ae62ae597bdb7d70808b94ba081a73c
RavikiranGoru <ravikirangoru@gmail.com>
2024-06-02 14:55:18 +0530

    read data from excel and filter based on column value implementation

--- batchJob.js
+++ batchJob.js
@@ -1,7 +1,47 @@
 const cron = require('node-cron');
 const winston = require('winston');
-require('dotenv').config();
 
+
+
+const xlsx = require('xlsx');
+const path = require('path');
+// Function to read Excel file and retrieve data based on matched value in a specific column
+const readExcelAndRetrieveData = (filePath, columnName, matchValue) => {
+    // Read the Excel file
+    const workbook = xlsx.readFile(filePath);
+
+    // Get the first sheet name
+    const sheetName = workbook.SheetNames[0];
+
+    // Get the worksheet
+    const worksheet = workbook.Sheets[sheetName];
+
+    // Convert the worksheet to JSON
+    const jsonData = xlsx.utils.sheet_to_json(worksheet);
+    console.log('Json Data:', jsonData);
+
+    // Find the rows that match the specified value in the specified column
+    const matchedRows = jsonData.filter(row => row[columnName] === matchValue);
+
+    return matchedRows;
+};
+
+// Example usage
+const filePath = path.join(__dirname, 'example.xlsx'); // Replace with your Excel file path
+const columnName = 'Status'; // Replace with your column name
+const matchValue = 'Active'; // Replace with the value you want to match
+
+const statusMatchedData = readExcelAndRetrieveData(filePath, columnName, matchValue);
+console.log('Status Matched Data:', statusMatchedData);
+
+
+const columnName2 = 'Name'; // Replace with your column name
+const matchValue2 = 'Bob'; // Replace with the value you want to match
+
+const nameMatchedData = readExcelAndRetrieveData(filePath, columnName2, matchValue2);
+console.log('Status Matched Data:', nameMatchedData);
+
+require('dotenv').config();
 // Now you can access environment variables using process.env
 const apiKey = process.env.API_KEY;
 
@@ -33,8 +73,12 @@ const runBatchJob = async () => {
 
 
 // Schedule the batch job to run every 10 seconds
-cron.schedule('*/10 * * * * *', () => {
+cron.schedule('0 0 * * * *', () => {
     logger.info('Batch job started.');
     runBatchJob();
 });
+
+// to run now
+logger.info(`Batch job started  now on env key ${apiKey}. `);
+runBatchJob();
 logger.info(`Batch job scheduler started on env key ${apiKey}. Waiting for the next run...`);
